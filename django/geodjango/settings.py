5e5c9559e0fe20c1ddddc0a49b664a9ffba620a8
Szymon Michniak <s.michniak@icloud.com>
2021-04-07 09:17:21 +0200

    Leaflet removed from django

--- django/geodjango/settings.py
+++ django/geodjango/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'leaflet',
     'world',
 ]
 
--- django/world/admin.py
+++ django/world/admin.py
@@ -1,16 +1,12 @@
 from django.contrib.gis import admin
-from leaflet.admin import LeafletGeoAdmin
 
 from .models import Marker
 
 
 # Register your models here.
 
-# @admin.register(Marker)
+@admin.register(Marker)
 class MarkerAdmin(admin.OSMGeoAdmin):
     """Marker admin."""
 
     list_display = ("name", "location")
-
-
-admin.site.register(Marker, LeafletGeoAdmin)
