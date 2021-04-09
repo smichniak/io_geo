3bf68a6db26efdf699109844d95fa571d1a10d8b
Hubert Grochowski <hg417878@students.mimuw.edu.pl>
2021-04-09 22:10:12 +0200

    hypsometric map generator implementation

--- prototypes/gen_hypso_map.py
+++ prototypes/gen_hypso_map.py
@@ -1,34 +1,96 @@
 #!/usr/bin/env python3
-
+from matplotlib.colors import ListedColormap
 from osgeo import gdal
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-os.system('eio clip -o Shasta-30m-DEM.tif --bounds 49.180417 20.085446 50.2 21.1')
 
-filename = "Shasta-30m-DEM.tif"
-gdal_data = gdal.Open(filename)
-gdal_band = gdal_data.GetRasterBand(1)
-nodataval = gdal_band.GetNoDataValue()
+def set_title(longitude1, latitude1, longitude2, latitude2):
+    title = 'Hypsometric map of ['
+    title += str(abs(latitude1))
+    if latitude1 >= 0.:
+        title += 'N '
+    else:
+        title += 'S '
 
-# convert to a numpy array
-data_array = gdal_data.ReadAsArray().astype(float)
+    title += str(abs(longitude1))
 
-# replace missing values if necessary
-if np.any(data_array == nodataval):
-    data_array[data_array == nodataval] = np.nan
+    if longitude1 >= 0.:
+        title += 'E, '
+    else:
+        title += 'W, '
 
-# Plot our data with Matplotlib's 'contourf'
-fig = plt.figure(figsize=(12, 8))
-ax = fig.add_subplot()
-plt.contourf(data_array, cmap="terrain",
-             levels=list(range(0, 5000, 250)))
-plt.title("Hypsometric map of ...(bounds)")
-cbar = plt.colorbar()
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+    title += str(abs(latitude2))
 
-print(data_array)
+    if latitude2 >= 0.:
+        title += 'N '
+    else:
+        title += 'S '
 
-#os.system("rm -rf *.tif")
+    title += str(abs(longitude2))
+
+    if longitude2 >= 0.:
+        title += 'E]'
+    else:
+        title += 'W]'
+
+    return title
+
+
+def get_cmap():
+    # set color map
+    # combine 'terrain' matplotlib color map with 'Greens' and 'hot'
+    terrain = plt.get_cmap('terrain')
+    greens = plt.get_cmap('PiYG')
+    hot = plt.get_cmap('hot')
+
+    terrain = terrain(np.linspace(0, 1, 256))
+    greens = greens(np.linspace(0, 1, 256))
+    hot = hot(np.linspace(0, 1, 256))
+
+    #terrain[:64, :] = greens[128:][::2]
+    terrain[208:] = hot[58:154][::2][::-1]
+
+    newcmap = ListedColormap(terrain)
+
+    return newcmap
+
+
+def get_levels(data_array, smooth_colouring):
+    max_ = np.where(data_array == data_array.max())
+    top = int(data_array[max_])
+    step = 250
+    if smooth_colouring:
+        step = 50
+    return list(range(0, top + 500, step))
+
+
+def gen_hypso_map(longitude1, latitude1, longitude2, latitude2, smooth_colouring):
+    os.system('eio clip -o my_DEM.tif --bounds '
+              + str(longitude1) + ' ' + str(latitude1) + ' '
+              + str(longitude2) + ' ' + str(latitude2))
+    filename = "my_DEM.tif"
+    gdal_data = gdal.Open(filename)
+    gdal_band = gdal_data.GetRasterBand(1)
+    nodataval = gdal_band.GetNoDataValue()
+
+    # convert to a numpy array
+    data_array = gdal_data.ReadAsArray().astype(float)
+
+    # replace missing values if necessary
+    if np.any(data_array == nodataval):
+        data_array[data_array == nodataval] = np.nan
+
+    # Plot our data with Matplotlib's 'contourf'
+    plt.figure(figsize=(12, 8))
+    plt.axis('off')
+    plt.contourf(data_array, cmap=get_cmap(), levels=get_levels(data_array, smooth_colouring))
+
+    plt.title(set_title(longitude1, latitude1, longitude2, latitude2))
+    plt.colorbar()
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.savefig('hypso_map.png')
+
+
+gen_hypso_map(-122.6, 41.15, -121.9, 41.6, 0)
