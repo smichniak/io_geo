6d63b712ce6ed28ce854d51d2ab7ace9549964c8
Michal <michaltraczyk943@gmail.com>
2021-05-29 18:01:53 +0200

    Shadowing in hypsometric maps.

--- django/world/python_scripts/gen_hypso_map.py
+++ django/world/python_scripts/gen_hypso_map.py
@@ -40,24 +40,37 @@ def get_levels(data_array, smooth_colouring):
     return list(range(0, top + 500, step))
 
 
+def hillshade(array, azimuth, angle_altitude):
+    x, y = np.gradient(array)
+    slope = np.pi / 2. - np.arctan(np.sqrt(x * x + y * y))
+    aspect = np.arctan2(-x, y)
+    azimuthrad = azimuth * np.pi / 180.
+    altituderad = angle_altitude * np.pi / 180.
+
+    shaded = np.sin(altituderad) * np.sin(slope) + np.cos(altituderad) * np.cos(slope) * np.cos(azimuthrad - aspect)
+
+    return 255 * (shaded + 1) / 2
+
+
 # E1/W1, N1/S1, E2/W2, N2/S2, 0/1 - steps/smooth
 def gen_hypso_map(longitude1, latitude1, longitude2, latitude2, smooth_colouring):
+    topocmap = 'Spectral_r'
     details_map = gen_details_map(longitude1, latitude1, longitude2, latitude2)
-
     data_array, hypso_map, hypso_map_database_url = get_dem_data(longitude1, latitude1,
                                                                  longitude2, latitude2)
+
     hypso_map += '.png'
     hypso_map_database_url += '.png'
     # Plot our data with Matplotlib's 'contourf'
-    plt.figure(figsize=(12, 8))
-    plt.axis('off')
-    plt.contourf(data_array, cmap=get_cmap(), levels=get_levels(data_array, smooth_colouring))
-
+    fig = plt.figure(figsize=(12, 8))
+    ax = fig.add_subplot(111)
+    ax.matshow(hillshade(data_array, 30, 30), cmap='Greys', alpha=.5, zorder=10)
+    cax = ax.contourf(data_array, cmap=topocmap, origin='image', levels=get_levels(data_array, smooth_colouring))
+    fig.colorbar(cax, ax=ax)
     title_start = 'Hypsometric map of ['
-    plt.title(set_title(title_start, longitude1, latitude1, longitude2, latitude2))
-    plt.colorbar()
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.savefig(hypso_map)
+    fig.suptitle(set_title(title_start, longitude1, latitude1, longitude2, latitude2))
+    fig.gca().set_aspect('equal', adjustable='box')
+    fig.savefig(hypso_map)
 
     saved_image = HypsometricImages.objects.create(image=hypso_map_database_url, details_map=details_map)
 
